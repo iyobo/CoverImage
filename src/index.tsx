d9cb73b3b20c8cf2ac39a04ac9f665e86ed1cb8b
iyobo <phil.eki@gmail.com>
2020-09-19 22:42:21 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface IProp {
+    imageUrl: string;
+    width?: string | number;
+    minWidth?: string | number;
+    height?: string | number;
+    minHeight?: string | number;
+    backgroundColor?: string;
+    style?: any;
+    className?: any;
+}
+
+
+export default function (props: IProp) {
+
+    const style = {
+        backgroundImage: `url("${props.imageUrl}")`,
+        backgroundSize: 'cover',
+        width: props.width || 100,
+        minWidth: props.minWidth||0,
+        height: props.height||80,
+        minHeight: props.minHeight||0,
+        backgroundColor: props.backgroundColor || '#7d8c92',
+        marginRight: '10px',
+        cursor: 'pointer',
+
+    };
+
+    return <div className={props.className} style={{...style, ...props.style}}/>;
+
+}
+
